45a70ad413bd37663803df9cd1138692da3101a2
Pinto Manuel <pinto@fireworkx.co.za>
2023-02-15 23:42:23 +0200

    Frontend commit setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,30 @@
+import 'react'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'jquery/dist/jquery.min.js';
+import 'popper.js/dist/popper.min.js';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+
+
+function App() {
+ 
+
+  return (
+    <div className="App">
+    <i className="fas fa-check"></i>
+    <div className="card">
+    <div className="card-body">
+      <h5 className="card-title">Card title</h5>
+      <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="#" className="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>
+  
+    </div>
+  )
+}
+
+export default App
