b9e56f64c34bd733f2a84db72868efbca4592125
Pinto Manuel <pinto@fireworkx.co.za>
2023-04-03 04:45:37 +0200

    Frontend folder structure basic setup is done

--- backend/src/routes/enrollment.js
+++ backend/src/routes/enrollment.js
@@ -1,15 +1,21 @@
+const { request } = require('express');
 const express = require('express');
 const router = express.Router();
 const enrollmentController = require('../controllers/enrollment');
+const authenticate = require('../middlewares/authenticate');
+const { authorize } = require('../middlewares/authorize')
 
+const allowedRolesEnrollment = ['teacher', 'admin']
+
+// Route grouping
 router.route('/')
-    .post(enrollmentController.createEnrollment)
-    .get(enrollmentController.getAllEnrollments);
+    .post(authenticate, authorize(allowedRolesEnrollment), enrollmentController.createEnrollment)
+    .get(authenticate, authorize(allowedRolesEnrollment), enrollmentController.getAllEnrollments);
 
 router.route('/:id')
-    .get(enrollmentController.getEnrollmentById)
-    .put(enrollmentController.updateEnrollment)
-    .delete(enrollmentController.deleteEnrollment);
+    .get(authenticate, authorize(allowedRolesEnrollment), enrollmentController.getEnrollmentById)
+    .put(authenticate, authorize(allowedRolesEnrollment), enrollmentController.updateEnrollment)
+    .delete(authenticate, authorize(allowedRolesEnrollment), enrollmentController.deleteEnrollment);
 
 router.route('/student/:studentId')
     .get(enrollmentController.getEnrollmentsByStudentId);
