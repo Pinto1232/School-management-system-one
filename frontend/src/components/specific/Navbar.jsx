648eb22bc05ecb9b985ff01717a9fc2c9b9d6b94
Pinto Manuel <pinto@fireworkx.co.za>
2023-04-05 19:58:56 +0200

    commit code

--- frontend/src/components/specific/Navbar.jsx
+++ frontend/src/components/specific/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Box, Flex, Spacer, Button, Link } from '@chakra-ui/react';
+import { Box, Flex, Spacer, Button, Link, IconButton } from '@chakra-ui/react';
 import { useColorMode } from '@chakra-ui/react';
 import { Link as RouterLink } from 'react-router-dom';
+import { SunIcon, MoonIcon } from '@chakra-ui/icons';
 
 const Navbar = () => {
   const { colorMode, toggleColorMode } = useColorMode();
 
-  
+
 
   return (
     <Box bg={colorMode === 'light' ? 'gray.100' : 'gray.700'} p={4}>
@@ -14,14 +15,16 @@ const Navbar = () => {
         <Link as={RouterLink} to="/" mr={4} style={{ textDecoration: 'none' }}>
           Home
         </Link>
-     
+
         <Link as={RouterLink} to="/register" mr={4} style={{ textDecoration: 'none' }}>
-          Signup
+          Sign Up
         </Link>
         <Spacer />
-        <Button onClick={toggleColorMode}>
-          {colorMode === 'light' ? 'Dark' : 'Light'} Mode
-        </Button>
+        <IconButton
+          onClick={toggleColorMode}
+          icon={colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+          aria-label="Toggle color mode"
+        />
       </Flex>
     </Box>
   );
