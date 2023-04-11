828aab974ee1b4055ac7d1de71e33f898233ef2e
Pinto Manuel <pinto@fireworkx.co.za>
2023-04-11 12:04:33 +0200

    Working on roles and permission

--- frontend/src/utils/acl.js
+++ frontend/src/utils/acl.js
@@ -3,10 +3,30 @@
 import { Acl } from 'react-acl';
 
 const permissions = {
-  admin: ['*'], // All permissions
-  manager: ['manage:students', 'manage:teachers', 'manage:classes'],
-  teacher: ['manage:attendance', 'manage:grading', 'manage:lessons'],
-  student: ['view:grades', 'view:lessons', 'view:calendar']
+    admin: ['*'], // All permissions
+    manager: [
+        'manage:students', 
+        'manage:teachers', 
+        'manage:classes', 
+        'manage:attendance',
+        'manage:classroom',
+        'manage:course',
+        'manage:faculty',
+        'manage:enrollment',
+    ],
+    teacher: [
+        'manage:attendance', 
+        'manage:grading', 
+        'manage:lessons',
+        'manage:classroom',
+        'manage:course'
+    ],
+    student: [
+        'view:grades', 
+        'view:lessons', 
+        'view:calendar',
+        'view:course',
+    ]
 };
 
 const acl = new Acl(permissions);
