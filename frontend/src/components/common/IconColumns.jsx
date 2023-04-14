51c58c75ff7fc4096eddef807575c122943cc3ff
Pinto Manuel <pinto@fireworkx.co.za>
2023-04-14 23:42:12 +0200

    4 rows icon component completed

--- frontend/src/components/common/IconColumns.jsx
+++ frontend/src/components/common/IconColumns.jsx
@@ -1,16 +1,13 @@
-// IconColumns.js
 import * as React from "react";
 import { SimpleGrid } from "@chakra-ui/react";
-import { FaHome, FaUserAlt, FaBell, FaCog } from "react-icons/fa";
 import IconColumn from "./IconColumn";
 
-const IconColumns = () => {
+const IconColumns = ({ iconsData }) => {
   return (
     <SimpleGrid bg="#3182ce" padding={40} columns={4} spacing={10}>
-      <IconColumn icon={FaHome} title="Multiple Auction Types" />
-      <IconColumn icon={FaUserAlt} title="Globally Available" />
-      <IconColumn icon={FaBell} title="Real Time Environment" />
-      <IconColumn icon={FaCog} title="24/7 Support" />
+      {iconsData.map((iconData) => (
+        <IconColumn key={iconData.id} icon={iconData.icon} title={iconData.title} />
+      ))}
     </SimpleGrid>
   );
 };
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,8 +3,16 @@ import Jumbotron from '../components/specific/Jumbotron';
 import bgImage from '../assets/images/background-01.jpg';
 import CardGrid from '../components/specific/CardGrid';
 import IconColumns from '../components/common/IconColumns';
+import { FaHome, FaUserAlt, FaBell, FaCog } from "react-icons/fa";
 
 
+const iconsData = [
+    { id: 1, icon: FaHome, title: "Multiple Auction Types" },
+    { id: 2, icon: FaUserAlt, title: "Globally Available" },
+    { id: 3, icon: FaBell, title: "Real Time Environment" },
+    { id: 4, icon: FaCog, title: "24/7 Support" },
+];
+  
 
 
 const Home = () => {
@@ -27,7 +35,11 @@ const Home = () => {
                 buttonOnClick={handleButtonClick}
             />
             <CardGrid />
-            <IconColumns />
+
+            {/* Icon component */}
+            <IconColumns 
+               iconsData={iconsData} 
+            />
         </div>
     )
 };
