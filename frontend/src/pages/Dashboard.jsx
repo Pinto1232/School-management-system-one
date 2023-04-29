e2f9fdf6fa208e62f08f770178927c1334c2555e
Pinto Manuel <pinto@fireworkx.co.za>
2023-04-29 17:40:12 +0200

    Working in the dashboard component'

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -19,35 +19,19 @@ import {
 } from "@chakra-ui/react";
 import { HamburgerIcon } from "@chakra-ui/icons";
 import { useNavigate } from "react-router-dom";
-import {
-  FaUsers,
-  FaChalkboardTeacher,
-  FaSchool,
-  FaTasks,
-  FaUserClock,
-  FaFileAlt,
-  FaCalendarAlt,
-  FaBookOpen,
-  FaComments,
-  FaBook,
-  FaCalendar,
-  FaGraduationCap,
-  FaUnlockAlt,
-} from "react-icons/fa";
 import { IoLogOut } from "react-icons/io5";
 import AdjustableColumnLayout from "../components/specific/twocolumns/AdjustableColumnLayout";
 import UserProfile from "../components/common/UserProfilePage";
-import Avatars from "../assets/images/logo.png";
 import ProductSorting from "../components/specific/ProductSorting";
 import ProductSortingOptionsData from "../data/ProductSortingOptionsData";
 import Breadcrumbs from "../components/specific/Breadcrumbs";
 import BreadcumberData from "../data/BreadcumberData";
-import BlogPost from "../components/common/BlogPost";
-import blogPostsData from "../data/blogPostsData";
 import TableWithIcons from "../components/specific/tableImage/TableWithIcons";
 import productsImagesData from "../data/productsImagesData";
 import CardCustom from "../components/common/CardCustom";
 import cardCustomData from "../data/cardCustomData";
+import menuItemsData from "../data/menuItemsData";
+
 
 const Dashboard = () => {
   const bgDashboard = useColorModeValue("gray.50", "gray.700");
@@ -60,6 +44,7 @@ const Dashboard = () => {
   const textColor = useColorModeValue("#4A5568", "#fff");
   const backgroundColor = useColorModeValue("#fff", "gray.700");
   const backgroundBorder = useColorModeValue("", "1px solid");
+  const breadCumberBg = useColorModeValue("", "gray.800");
 
   /* console.log('User from context:', user); */
 
@@ -83,51 +68,7 @@ const Dashboard = () => {
     navigate("/");
   };
 
-  const menuItems = [
-    { label: "Student Management", path: "/student-management", icon: FaUsers },
-    {
-      label: "Teacher Management",
-      path: "/teacher-management",
-      icon: FaChalkboardTeacher,
-    },
-    { label: "Class Management", path: "/class-management", icon: FaSchool },
-    {
-      label: "Attendance Tracking",
-      path: "/attendance-tracking",
-      icon: FaUserClock,
-    },
-    {
-      label: "Grading and Assessment",
-      path: "/grading-assessment",
-      icon: FaFileAlt,
-    },
-    {
-      label: "Timetable Management",
-      path: "/timetable-management",
-      icon: FaCalendarAlt,
-    },
-    { label: "Lesson Planning", path: "/lesson-planning", icon: FaBookOpen },
-    {
-      label: "Parent Communication",
-      path: "/parent-communication",
-      icon: FaComments,
-    },
-    { label: "Library Management", path: "/library-management", icon: FaBook },
-    { label: "School Calendar", path: "/school-calendar", icon: FaCalendar },
-    {
-      label: "Online Learning Integration",
-      path: "/online-learning",
-      icon: FaGraduationCap,
-    },
-    { label: "Events", path: "/events", icon: FaCalendarAlt },
-    { label: "Task", path: "/tasks", icon: FaTasks },
-    {
-      label: "User Permissions and Roles",
-      path: "/user-permissions-roles",
-      icon: FaUnlockAlt,
-    },
-  ];
-
+  
   const handleNavigation = (path) => {
     navigate(path);
     onClose();
@@ -142,8 +83,8 @@ const Dashboard = () => {
   };
 
   return (
-    <Box bg={bgDashboard} minH="100vh">
-      <Flex direction="column" maxW="8xl" mx="auto" p={4}>
+    <Box bg={bgDashboard}  minH="100vh">
+      <Flex direction="column" maxW={'1290px'} mx="auto" p={4}>
         <Flex justify="space-between" align="center" mb={4}>
           <Heading>
             {/* <Text fontSize="3xl">PintoEdu Management</Text> */}
@@ -174,10 +115,11 @@ const Dashboard = () => {
         <Text color={textColor} fontSize="3xl" fontWeight={"bold"}>
           Dashboard
         </Text>
+        {/* Breadcrumbs */}
         <Box bg={backgroundColor} color={textColor}>
           <Breadcrumbs
             items={BreadcumberData}
-            bgColor={"gray.50"}
+            bgColor={breadCumberBg}
             BreadcPadding={2}
           />
         </Box>
@@ -207,7 +149,7 @@ const Dashboard = () => {
                   <CardCustom
                     data={cardCustomData}
                     paragraphWidth="200px"
-                    rows={5}
+                    rows={4}
                     columns={1}
                   />
                   {/* End of custom card */}
@@ -232,7 +174,7 @@ const Dashboard = () => {
                 maxW="4xl"
                 textAlign={"center"}
                 mx="auto"
-                p={3}
+                p={4}
                 borderWidth={0}
                 rounded="md"
                 border={2}
@@ -250,7 +192,7 @@ const Dashboard = () => {
                 maxW="4xl"
                 textAlign={"center"}
                 mx="auto"
-                p={3}
+                p={4}
                 borderWidth={0}
                 rounded="md"
                 border={2}
@@ -317,7 +259,7 @@ const Dashboard = () => {
           <DrawerHeader borderBottomWidth="1px">Dashboard Menu</DrawerHeader>
           <DrawerBody>
             <VStack spac ing={4} align="stretch">
-              {menuItems.map((item) => (
+              {menuItemsData.map((item) => (
                 <Flex
                   key={item.label}
                   fontSize="lg"
