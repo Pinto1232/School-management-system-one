ce215c02eb87e8f63252991161bcc7daa15842fe
Pinto Manuel <pinto@fireworkx.co.za>
2023-06-03 12:38:57 +0200

    Commit code

--- frontend/src/data/menuItemsData.js
+++ frontend/src/data/menuItemsData.js
@@ -1,46 +1,98 @@
-import { FaBook, FaBookOpen, FaCalendar, FaCalendarAlt, FaChalkboardTeacher, FaComments, FaFileAlt, FaGraduationCap, FaSchool, FaTasks, FaUnlockAlt, FaUserClock, FaUsers } from "react-icons/fa";
+import {
+  FaBook,
+  FaBookOpen,
+  FaCalendar,
+  FaCalendarAlt,
+  FaChalkboardTeacher,
+  FaComments,
+  FaFileAlt,
+  FaGraduationCap,
+  FaSchool,
+  FaTasks,
+  FaUnlockAlt,
+  FaUserClock,
+  FaUsers,
+} from "react-icons/fa";
 
-export default  [
-    { label: "Student Management", path: "/student-management", icon: FaUsers },
-    {
-      label: "Teacher Management",
-      path: "/teacher-management",
-      icon: FaChalkboardTeacher,
-    },
-    { label: "Class Management", path: "/class-management", icon: FaSchool },
-    {
-      label: "Attendance Tracking",
-      path: "/attendance-tracking",
-      icon: FaUserClock,
-    },
-    {
-      label: "Grading and Assessment",
-      path: "/grading-assessment",
-      icon: FaFileAlt,
-    },
-    {
-      label: "Timetable Management",
-      path: "/timetable-management",
-      icon: FaCalendarAlt,
-    },
-    { label: "Lesson Planning", path: "/lesson-planning", icon: FaBookOpen },
-    {
-      label: "Parent Communication",
-      path: "/parent-communication",
-      icon: FaComments,
-    },
-    { label: "Library Management", path: "/library-management", icon: FaBook },
-    { label: "School Calendar", path: "/school-calendar", icon: FaCalendar },
-    {
-      label: "Online Learning Integration",
-      path: "/online-learning",
-      icon: FaGraduationCap,
-    },
-    { label: "Events", path: "/events", icon: FaCalendarAlt },
-    { label: "Task", path: "/tasks", icon: FaTasks },
-    {
-      label: "User Permissions and Roles",
-      path: "/user-permissions-roles",
-      icon: FaUnlockAlt,
-    },
-  ];
+export default [
+  { 
+    label: "Dashboard", 
+    path: "/dashboard",
+    icon: FaUsers 
+  },
+  {
+    label: "Students",
+    path: "/student",
+    icon: FaChalkboardTeacher,
+  },
+  { 
+    label: "Teachers", 
+    path: "/teachers", 
+    icon: FaSchool 
+  },
+  {
+    label: "Courses",
+    path: "/courses",
+    icon: FaUserClock,
+  },
+  {
+    label: "Attendance",
+    path: "/attendance",
+    icon: FaFileAlt,
+  },
+  {
+    label: "Calendar/Events",
+    path: "/events",
+    icon: FaCalendarAlt,
+  },
+  { 
+    label: "Lesson Planning", 
+    path: "/lesson-planning", 
+    icon: FaBookOpen 
+  },
+  {
+    label: "Admissions",
+    path: "/admissions",
+    icon: FaComments,
+  },
+  { 
+    label: "Reports", 
+    path: "/reports", 
+    icon: FaBook 
+  },
+  { 
+    label: "Fees Management", 
+    path: "/fees", 
+    icon: FaCalendar 
+  },
+  {
+    label: "Grading",
+    path: "/grading",
+    icon: FaGraduationCap,
+  },
+  { 
+    label: "Events", 
+    path: "/events", 
+    icon: FaCalendarAlt 
+  },
+  { 
+    label: "Task", 
+    path: "/tasks", 
+    icon: FaTasks 
+  },
+  {
+    label: "Parent Portal",
+    path: "/parent_portal",
+    icon: FaUnlockAlt,
+  },
+  {
+    label: "Staff Management",
+    path: "/staff_management",
+    icon: FaUnlockAlt,
+  },
+  {
+    label: "Timetable Management",
+    path: "/timetable_management",
+    icon: FaUnlockAlt,
+  },
+];
