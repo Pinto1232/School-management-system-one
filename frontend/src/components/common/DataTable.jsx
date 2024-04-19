9a177101196cef0a37c55f27d89ef332dabfdfb8
Pinto Manuel <pintoydc@gmail.com>
2024-04-20 01:33:30 +0200

    toast is now perfectly working

--- frontend/src/components/common/DataTable.jsx
+++ frontend/src/components/common/DataTable.jsx
@@ -12,24 +12,25 @@ import {
   useToast,
   Text,
   Flex,
-} from '@chakra-ui/react';
-import { useMemo, useState } from 'react';
-import { useTable, useSortBy } from 'react-table';
-import { FaSort, FaSortUp, FaSortDown, FaTrash, FaEye } from 'react-icons/fa';
-import axios from 'axios';
-import { useUserContext } from '../../contexts/UserContext';
-import UserDetailsModal from './UserDetailsModal';
+} from '@chakra-ui/react'
+import { useMemo, useState } from 'react'
+import { useTable, useSortBy } from 'react-table'
+import { FaSort, FaSortUp, FaSortDown, FaTrash, FaEye, FaExclamationTriangle } from 'react-icons/fa'
+import axios from 'axios'
+import { useUserContext } from '../../contexts/UserContext'
+import UserDetailsModal from './UserDetailsModal'
+import { motion } from 'framer-motion'
 
 const DataTable = ({ data, fetchData }) => {
-  const { user } = useUserContext();
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [selectedUser, setSelectedUser] = useState(null);
-  const toast = useToast();
+  const { user } = useUserContext()
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [selectedUser, setSelectedUser] = useState(null)
+  const toast = useToast()
 
   const handleView = (user) => {
-    setSelectedUser(user);
-    setIsModalOpen(true);
-  };
+    setSelectedUser(user)
+    setIsModalOpen(true)
+  }
 
   const columns = useMemo(
     () => [
@@ -38,8 +39,12 @@ const DataTable = ({ data, fetchData }) => {
         accessor: 'image',
         Cell: ({ value }) => {
           const imageUrl = value
-            ? `http://localhost:3001/api/users/uploads/${value.split('\\').pop().split('/').pop()}`
-            : undefined;
+            ? `http://localhost:3001/api/users/uploads/${value
+                .split('\\')
+                .pop()
+                .split('/')
+                .pop()}`
+            : undefined
           return (
             <Box>
               {imageUrl ? (
@@ -54,7 +59,7 @@ const DataTable = ({ data, fetchData }) => {
                 <span>No image</span>
               )}
             </Box>
-          );
+          )
         },
       },
       {
@@ -71,67 +76,96 @@ const DataTable = ({ data, fetchData }) => {
       },
     ],
     []
-  );
+  )
 
   const handleDelete = async (id) => {
     const toastId = toast({
-      position: "bottom",
+      position: 'bottom',
+      duration: 3500,
       render: () => (
-        <Box
-          width="100%"
-          position="relative"
-          bottom={0}
-          color="white"
-          p={2}
-          bg="red.500"
-          borderRadius="md"
+        <motion.div
+          initial={{ x: -30, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{
+            x: {
+              type: 'spring',
+              stiffness: 900,
+              damping: 12,
+              repeat: 1,
+              repeatType: 'reverse',
+              duration: 0.15,
+            },
+            opacity: { duration: 0.1 },
+          }}
         >
-          <Flex justifyContent="space-between" alignItems="center">
-            <Text>Are you sure?</Text>
-            <ButtonGroup size="sm">
-              <Button
-                colorScheme="green"
-                onClick={async () => {
-                  toast.close(toastId);
-                  try {
-                    await axios.delete(`http://localhost:3001/api/users/${id}`);
-                    fetchData();
-                  } catch (error) {
-                    console.error('Delete error:', error);
-                  }
-                }}
-              >
-                Yes
-              </Button>
-              <Button colorScheme="red" onClick={() => toast.close(toastId)}>
-                No
-              </Button>
-            </ButtonGroup>
-          </Flex>
-        </Box>
+          <Box
+            width="100vw"
+            maxWidth="1300px"
+            position="fixed"
+            left="50%"
+            transform="translateX(-50%)"
+            bottom="10px"
+            color="white"
+            p={3}
+            bg="red.500"
+            borderRadius="md"
+            boxShadow="0px 4px 10px rgba(0, 0, 0, 0.1)"
+          >
+            <Flex justifyContent="space-between" alignItems="center">
+              <Flex alignItems="center">
+                <Box pr={2}>
+                  <FaExclamationTriangle color="yellow" size="24px" />
+                </Box>
+                <Text>Warning: This Action Cannot Be Undone!</Text>
+              </Flex>
+              <ButtonGroup size="sm">
+                <Button
+                  colorScheme="teal"
+                  onClick={async () => {
+                    toast.close(toastId)
+                    try {
+                      await axios.delete(
+                        `http://localhost:3001/api/users/${id}`
+                      )
+                      fetchData()
+                    } catch (error) {
+                      console.error('Delete error:', error)
+                    }
+                  }}
+                >
+                  Yes
+                </Button>
+                <Button colorScheme="red" onClick={() => toast.close(toastId)}>
+                  No
+                </Button>
+              </ButtonGroup>
+            </Flex>
+          </Box>
+        </motion.div>
       ),
-    });
-  };
-
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({ columns, data }, useSortBy);
+    })
+  }
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({ columns, data }, useSortBy)
 
   // Pagination state
-  const [currentPage, setCurrentPage] = useState(0);
-  const itemsPerPage = 6;
-  const totalPages = Math.ceil(rows.length / itemsPerPage);
+  const [currentPage, setCurrentPage] = useState(0)
+  const itemsPerPage = 6
+  const totalPages = Math.ceil(rows.length / itemsPerPage)
 
   const handlePreviousPage = () => {
-    setCurrentPage((prev) => (prev > 0 ? prev - 1 : prev));
-  };
+    setCurrentPage((prev) => (prev > 0 ? prev - 1 : prev))
+  }
 
   const handleNextPage = () => {
-    setCurrentPage((prev) => (prev < totalPages - 1 ? prev + 1 : prev));
-  };
+    setCurrentPage((prev) => (prev < totalPages - 1 ? prev + 1 : prev))
+  }
 
   const currentTableBody = rows.slice(
     currentPage * itemsPerPage,
     (currentPage + 1) * itemsPerPage
-  );
+  )
 
   return (
     <Box width="100%" overflowX={{ base: 'scroll', md: 'auto' }}>
@@ -166,9 +200,9 @@ const DataTable = ({ data, fetchData }) => {
         </Thead>
         <Tbody {...getTableBodyProps()} bg="white">
           {currentTableBody.map((row, rowIndex) => {
-            prepareRow(row);
-            const isOddRow = rowIndex % 2 !== 0;
-            const bgColor = isOddRow ? 'gray.50' : 'white';
+            prepareRow(row)
+            const isOddRow = rowIndex % 2 !== 0
+            const bgColor = isOddRow ? 'gray.50' : 'white'
             return (
               <Tr {...row.getRowProps()} _hover={{ bg: 'gray.100' }}>
                 {row.cells.map((cell) => (
@@ -201,7 +235,7 @@ const DataTable = ({ data, fetchData }) => {
                   </ButtonGroup>
                 </Td>
               </Tr>
-            );
+            )
           })}
         </Tbody>
       </Table>
@@ -211,11 +245,16 @@ const DataTable = ({ data, fetchData }) => {
             <Button onClick={handlePreviousPage} disabled={currentPage === 0}>
               Previous
             </Button>
-            <Button onClick={handleNextPage} disabled={currentPage === totalPages - 1}>
+            <Button
+              onClick={handleNextPage}
+              disabled={currentPage === totalPages - 1}
+            >
               Next
             </Button>
           </ButtonGroup>
-          <Text>Page {currentPage + 1} of {totalPages}</Text>
+          <Text>
+            Page {currentPage + 1} of {totalPages}
+          </Text>
         </Box>
       )}
       <UserDetailsModal
@@ -225,7 +264,7 @@ const DataTable = ({ data, fetchData }) => {
         modalWidth="800px"
       />
     </Box>
-  );
-};
+  )
+}
 
-export default DataTable;
+export default DataTable
